52007c50cd059a8dc353ae1868745400a4bb935d
m-piqueras <mario.piqueras.miro@gmail.com>
2024-12-08 17:54:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const contactButton = document.querySelector('#contact button');
+    contactButton.addEventListener('click', () => {
+        alert('¡Gracias por tu interés! Me pondré en contacto contigo pronto.');
+    });
+});
