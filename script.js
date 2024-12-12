c228fda0bb9da8ef66b3285e3cd3eeef93f7e205
m-piqueras <mario.piqueras.miro@gmail.com>
2024-12-12 20:03:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
         alert('¡Gracias por tu interés! Me pondré en contacto contigo pronto.');
     });
 });
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const collapsibles = document.querySelectorAll(".collapsible");
+        collapsibles.forEach((button) => {
+            button.addEventListener("click", function () {
+                this.classList.toggle("active");
+                const content = this.nextElementSibling;
+                if (content.style.display === "block") {
+                    content.style.display = "none";
+                } else {
+                    content.style.display = "block";
+                }
+            });
+        });
+    });
+</script>
