08ad61742b0a942003c65bcedacbbce3ac0cf2a5
m-piqueras <mario.piqueras.miro@gmail.com>
2024-12-13 12:27:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,10 @@ const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach(button => {
     button.addEventListener("click", () => {
         const content = button.nextElementSibling;
-        button.classList.toggle("active");
-
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
+        content.classList.toggle("active");
+        content.style.maxHeight = content.classList.contains("active")
+            ? content.scrollHeight + "px"
+            : null;
     });
 });
 
-// Manejo de carga de PDFs
-const pdfLinks = document.querySelectorAll(".load-pdf");
-
-pdfLinks.forEach(link => {
-    link.addEventListener("click", (event) => {
-        event.preventDefault();
-        const pdfSrc = link.getAttribute("data-pdf");
-        const viewer = link.nextElementSibling;
-
-        viewer.src = pdfSrc;
-        viewer.style.height = "400px";
-    });
-});
